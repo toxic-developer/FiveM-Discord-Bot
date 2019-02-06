c759898444ea9987e00a6b6da4135764e66f0392
GrimDesigns <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-02-05 22:01:23 -0700

    Add "FiveMToDiscord Bot By GrimDesigns" To Update Check

--- systeminfo/SERVER/Server.lua
+++ systeminfo/SERVER/Server.lua
@@ -267,7 +267,7 @@ PerformHttpRequest('https://raw.githubusercontent.com/GrimDesignsFiveM/FiveMToDi
 	PerformHttpRequest('https://raw.githubusercontent.com/GrimDesignsFiveM/FiveMToDiscord/master/' .. GithubResourceName .. '/CHANGES', function(Error, Changes, Header)
 		print('\n')
 		print('##############')
-		print('## ' .. GetCurrentResourceName())
+		print('## FiveMToDiscord Bot By GrimDesigns ')
 		print('##')
 		print('## Current Version: ' .. CurrentVersion)
 		print('## Newest Version: ' .. NewestVersion)
